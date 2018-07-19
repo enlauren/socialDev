d08b27a3ce2657ce5142e2c7f7d3f85efd759320
Laurentiu Enache <enlau101@gmail.com>
2018-07-19 23:30:17 +0100

    initial express server with route files

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://laurentiu:analiza1@ds247310.mlab.com:47310/social_dev_connector'
+    mongoURI: 'mongodb://laurentiu:analiza1@ds247310.mlab.com:47310/social_dev_connector',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport')
 
+// main dependencies
+        /*  express             main framework
+            mongoose            connect and interact with mongoDB
+            passport            used for authentification
+            passport-jwt        jason web token
+            jsonwebtoken        used to actually generate the web token
+            bodyparser          to take data through our requests
+            bcryptjs            for passwords
+            validator           for validations
 
+npm i express mongoose passport passport-jwt jsonwebtoken bodyparser bcryptjs validator
+
+npm i -D nodemon        watch for code changes
+        */
 
 const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
 mongoose.connect(db).then(()=>console.log('mongo db connected')).catch(err=>console.log(err));
 
-app.get('/', (req, res)=>res.send('hello world'));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 
 // use routes
